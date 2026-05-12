03054a66d23f16904a06a7be4b64a5ad66014bb5
judev <76juandevi@gmail.com>
2026-05-12 16:45:59 +0200

    encapsulamiento

--- main.py
+++ main.py
@@ -1,29 +1,75 @@
+from typing import Protocol
+
+from exceptions import LibroNoDisponibleError
+
+
+class LibroProtocol(Protocol):
+    def prestar(self) -> str:
+        """Método de prestar un libro"""
+        ...
+
+    def devolver(self) -> str:
+        """Método de devolver un libro"""
+        ...
+
+    def calcular_duracion(self) -> str:
+        """Calcula el tiempo de prestamo"""
+        ...
+
+
 class Libro:
-    pass
-    def __init__(self, titulo, autor, isbn, available):
+    def __init__(self, titulo, autor, isbn, disponible=True):
         self.titulo = titulo
         self.autor = autor
         self.isbn = isbn
-        self.available = available
-        self.lended_count = 0
+        self.disponible = disponible
+        # Variable privada cuando empieza con guion bajo, no es privada pero es buena practica para manejar las privadas, con 2 veces guion bajo es privado
+        self.__veces_prestado = 0
 
-        def __str__(self):
-            return f"{self.titulo} por {self.autor} (ISBN: {self.isbn}) - {'Disponible' if self.available else 'No disponible'}"
+    @classmethod
+    def crear_no_disponible(cls, titulo, autor, isbn):
+        return cls(titulo, autor, isbn, disponible=False)
 
-        def cambiar_disponibilidad(self, disponible):
-            if self.available:
-                self.available = False
-            return "El libro no está disponible para prestar."
-        
-        def devolver(self):
-            self.available = True
-        return "El libro ha sido devuelto y ahora está disponible."
+    def __str__(self):
+        return f"{self.titulo} por {self.autor} disponible: {self.disponible}"
 
-mi_libro = Libro("Cien Años de Soledad", "Gabriel García Márquez", "978-84-376-0494-7", True)
-otro_libro = Libro("El Coronel no tiene quien le escriba", "Gabriel García Márquez", "978-84-376-0495-4", True)
+    def prestar(self):
+        if not self.disponible:
+            raise LibroNoDisponibleError(f"'{self.titulo}' no está disponible")
 
-mi_libro.prestar()
-mi_libro.devolver()
+        if self.disponible:
+            self.disponible = False
+            self.__veces_prestado += 1
+            return f"'{self.titulo}' prestado exitosamente. Total préstamos: {self.__veces_prestado}"
 
-print(mi_libro.titulo)
-print(mi_libro.autor)
+    def devolver(self):
+        self.disponible = True
+        return f"'{self.titulo}' devuelto y disponible nuevamente"
+
+    @property
+    def es_popular(self):
+        return self.__veces_prestado > 5
+
+    @property
+    def veces_prestado(self):
+        return self.__veces_prestado
+
+    @veces_prestado.setter
+    def veces_prestado(self, veces_prestado):
+        if veces_prestado > 0:
+            self.__veces_prestado = veces_prestado
+        raise ValueError("El valor de veces_prestado debe ser mayor a cero")
+
+    @property
+    def descripcion_completa(self):
+        return f"{self.titulo} por {self.autor} (ISBN: {self.isbn})"
+
+
+class LibroFisico(Libro):
+    def calcular_duracion(self):
+        return "7 días"
+
+
+class LibroDigital(Libro):
+    def calcular_duracion(self):
+        return "14 días"
