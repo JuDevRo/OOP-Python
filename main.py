816af1ffbea66115fe2ba6c65fe8527c10da9504
judev <76juandevi@gmail.com>
2026-05-12 18:49:06 +0200

    exceptions

--- main.py
+++ main.py
@@ -1,75 +1,38 @@
-from typing import Protocol
+from exceptions import LibroNoDisponibleError, UsuarioNoEncontradoError
+from persistencia import Persistencia
 
-from exceptions import LibroNoDisponibleError
-
-
-class LibroProtocol(Protocol):
-    def prestar(self) -> str:
-        """Método de prestar un libro"""
-        ...
-
-    def devolver(self) -> str:
-        """Método de devolver un libro"""
-        ...
-
-    def calcular_duracion(self) -> str:
-        """Calcula el tiempo de prestamo"""
-        ...
+persistencia = Persistencia()
+biblioteca = persistencia.cargar_datos()
 
 
-class Libro:
-    def __init__(self, titulo, autor, isbn, disponible=True):
-        self.titulo = titulo
-        self.autor = autor
-        self.isbn = isbn
-        self.disponible = disponible
-        # Variable privada cuando empieza con guion bajo, no es privada pero es buena practica para manejar las privadas, con 2 veces guion bajo es privado
-        self.__veces_prestado = 0
+print("Bienvenido a Biblioteca")
 
-    @classmethod
-    def crear_no_disponible(cls, titulo, autor, isbn):
-        return cls(titulo, autor, isbn, disponible=False)
+print("Libros disponibles:")
+for libro in biblioteca.libros_disponibles():
+    print(libro.descripcion_completa)
+print()
 
-    def __str__(self):
-        return f"{self.titulo} por {self.autor} disponible: {self.disponible}"
+cedula = input("Digite el numero cedula: ")
+try:
+    usuario = biblioteca.buscar_usuario(cedula)
+    print(usuario.cedula, usuario.nombre)
+except UsuarioNoEncontradoError as e:
+    print(e)
 
-    def prestar(self):
-        if not self.disponible:
-            raise LibroNoDisponibleError(f"'{self.titulo}' no está disponible")
+titulo = input("Digite el titulo del libro: ")
+try:
+    libro = biblioteca.buscar_libro(titulo)
+    print(f"El libro que selecionaste es: {libro}")
+except LibroNoDisponibleError as e:
+    print(e)
 
-        if self.disponible:
-            self.disponible = False
-            self.__veces_prestado += 1
-            return f"'{self.titulo}' prestado exitosamente. Total préstamos: {self.__veces_prestado}"
+resultado = usuario.solicitar_libro(libro.titulo)
+print(f"\n{resultado}")
 
-    def devolver(self):
-        self.disponible = True
-        return f"'{self.titulo}' devuelto y disponible nuevamente"
+try:
+    resultado_prestar = libro.prestar()
+    print(f"\n{resultado_prestar}")
+except LibroNoDisponibleError as e:
+    print(e)
 
-    @property
-    def es_popular(self):
-        return self.__veces_prestado > 5
-
-    @property
-    def veces_prestado(self):
-        return self.__veces_prestado
-
-    @veces_prestado.setter
-    def veces_prestado(self, veces_prestado):
-        if veces_prestado > 0:
-            self.__veces_prestado = veces_prestado
-        raise ValueError("El valor de veces_prestado debe ser mayor a cero")
-
-    @property
-    def descripcion_completa(self):
-        return f"{self.titulo} por {self.autor} (ISBN: {self.isbn})"
-
-
-class LibroFisico(Libro):
-    def calcular_duracion(self):
-        return "7 días"
-
-
-class LibroDigital(Libro):
-    def calcular_duracion(self):
-        return "14 días"
+persistencia.guardar_datos(biblioteca)
